2a5872331c79da65e72b71086cdd1e8d53e3fad9
Divyansh_jain <jaindivyansh004@gmail.com>
2025-04-03 19:36:12 +0530

    Initial commit

---
+++ AcademicPerformanceAPI/api.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import pickle
+import numpy as np
+
+with open("Social_Media_Impact_on_Academic_Work_Performance.pkl", "rb") as f:
+    model = pickle.load(f)
+
+app = FastAPI()
+
+@app.post("/predict")
+def predict(features: list):
+    features_array = np.array(features).reshape(1, -1)
+    prediction = model.predict(features_array)
+    return {"prediction": prediction.tolist()}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8004)
